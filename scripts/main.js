68e71c929c9496cac491a3877c76873fc58259b0
jadrum <justindrum12@gmail.com>
2016-06-07 14:20:20 -0400

    WIP... got the board working, next going to attempt to make it save the posts

--- scripts/main.js
+++ scripts/main.js
@@ -11,39 +11,92 @@ var createBrowserHistory = require('history/lib/createBrowserHistory');
 /* app */
 
 var MessageMaker = React.createClass({
+  getInitialState: function() {
+    return {
+      messages : {}
+    }
+  },
+  addMessage: function(message) {
+    var time = (new Date()).getTime();
+    this.state.messages['message-' + time] = message;
+    this.setState({messages: this.state.messages});
+  
+  },
+  loadSamples : function() {
+    this.setState({
+      messages : require('./sample-messages')
+    });
+  },
+
+  renderMessage : function(key) {
+    return <Message key={key} index={key} details={this.state.messages[key]}/> 
+  },
+
   render: function() {
     return (
       <div className="lets-talk">
       
-        <Input />
-        <Board />
+        <div className="board">
+          <h1>Message Board</h1>
+          <ul className="message-list">
+            {Object.keys(this.state.messages).map(this.renderMessage)}
+          </ul>
+          <Input addMessage={this.addMessage} loadSamples={this.loadSamples}/>
+        </div>
+
       </div>
     )
   }
 });
 
+/* 
+  The message itself
+  <Message />
+*/
+var Message = React.createClass({
+  render: function() {
+    var details=this.props.details;
+    return (
+      <li className="msg-list">
+        <p>{details.author}: {details.comment}</p>        
+     </li>
+    )
+  }
+});
+
+
 /* Message Input */
 var Input = React.createClass({
+  addComment: function(event) {
+    // Stop form from submitting
+    event.preventDefault();
+
+    // Take info from form to make obj
+    var message = {
+      author: this.refs.author.value,
+      comment: this.refs.message.value
+    }
+
+    // Add fish to app state
+    this.props.addMessage(message);
+    this.refs.messageForm.reset();
+  },
   render: function() {  
     return (
-      <form className="input">
-        <h2>Please leave A message</h2>
-        <input type="text" ref="author" required />
-        <input type="text" ref="message" required />
-        <input type="Submit" />
-      </form>
+      <div className="form">
+        <form className="input" ref="messageForm" onSubmit={this.addComment}>
+          <label for="author">Author </label>
+          <input type="text" ref="author" required />
+
+          <label for="message">Message</label>
+          <input type="text" ref="message" required />
+          <button type="Submit">Submit Message</button>
+        </form>
+        <button onClick={this.props.loadSamples}>Load previous messages</button>
+      </div>  
     )
   }
 });
 
-/* Message board */
-
-var Board = React.createClass({
-  render: function() {
-    return (
-      <p>Message Board</p>
-    )
-  }
-});
 
 ReactDOM.render(<MessageMaker/>, document.querySelector('#main'));
---
+++ scripts/sample-messages.js
@@ -0,0 +1,13 @@
+// sample messages to find out how to load them on the message board
+
+module.exports = {
+  message1 : {
+    author: 'bob',
+    comment: 'how is everyone doing today?'
+  },
+
+  message2 : {
+    author: 'joe',
+    comment: 'im doing great bob! howre you?'
+  }
+};
